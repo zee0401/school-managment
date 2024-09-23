ebb0b66b8bea73fc5d77f731e8fadc9fa844adf0
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-09-23 18:01:15 +0530

    user card created

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,22 @@
+import UserCards from "@/components/UserCards";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="flex flex-col md:flex-row gap-4 p-4">
+      {/* Left */}
+      <div className="w-full lg:w-[75%] md:w-[80%] flex flex-col gap-8">
+        <div className="flex gap-4 justify-between flex-wrap-reverse">
+          <UserCards type="teacher" />
+          <UserCards type="student" />
+          <UserCards type="parent" />
+          <UserCards type="admin" />
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className="w-full  lg:w-[25%] md:w-[30%]">right</div>
+    </div>
+  );
 };
 
 export default AdminPage;
