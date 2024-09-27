097921b188a345d0882713dc9ce236d31f507664
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-09-27 18:02:25 +0530

    added

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -52,7 +52,7 @@ const renderRow = (item: StudentList) => (
         alt=""
         width={40}
         height={40}
-        className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        className="md:hidden xl:block w-10 h-10 rounded-full object-cover full"
       />
       <div className="flex flex-col">
         <h3 className="font-semibold">{item.name}</h3>
