38aeb541677b683b9e17590a3dc455a77ad84fef
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-10-05 17:56:32 +0530

    utils for get Latest monday

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+const getLatestMonday = (): Date => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+  const latestMonday = today;
+  latestMonday.setDate(today.getDate() - daysSinceMonday);
+  return latestMonday;
+};
